96ba05087667803e32b28db3ad2ffd7a00f3c6d2
Dave Burke <dburke84@gmail.com>
2020-09-17 17:43:20 -0500

    Use notify service instead of mutt for mdadm warnings

--- roles/raid_storage/templates/mdadm_warning.sh
+++ roles/raid_storage/templates/mdadm_warning.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-recipient="{{email_recipient}}"
-
 event="${1}"
 device="${2}"
 component="${3}"
@@ -18,5 +16,8 @@ message+="$(mdadm --detail /dev/disk/by-uuid/{{storage_uuid}})\n\n"
 message+="$(df -h | head -n 1)\n"
 message+="$(df -h | grep /dev/md)"
 
-echo -e "${message}" | mutt -s "${subject}" "${recipient}"
-
+curl -X POST \
+	-H "Content-Type: application/json" \
+	-H "Authorization: Basic $(echo -n "{{raid_notify_user}}:{{raid_notify_password}}" \
+	-d '{ "subject": "'"${subject}"'", "body": "'"${message}"'" }' \
+	"{{raid_notify_endpoint}}"
