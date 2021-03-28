cd0198d0efd7e6a3e0aff985b93b32b2c5d89865
Dave Burke <dburke84@gmail.com>
2021-03-28 11:29:14 -0500

    Comment out deprecated option
    I'll need to figure out the replacement eventually.

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 set -e
 
 export ANSIBLE_VAULT_PASSWORD_FILE=~/.ansible-vault-key
-export ANSIBLE_STDOUT_CALLBACK=actionable # Only log changed/failed
+#export ANSIBLE_STDOUT_CALLBACK=actionable # Only log changed/failed
 
 cd $(dirname ${0})
 ansible-playbook site.yml -i hosts -u root $@
