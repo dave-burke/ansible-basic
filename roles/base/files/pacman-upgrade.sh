7377381a6930bb4ea539e83da024ad446fe02425
Dave Burke <dburke84@gmail.com>
2021-06-13 11:47:54 -0500

    Update pacman-upgrade.sh to handle vanilla arch

--- roles/base/files/pacman-upgrade.sh
+++ roles/base/files/pacman-upgrade.sh
@@ -10,19 +10,45 @@ require() {
 	fi
 }
 
-require pikaur
-require pacman-mirrors
-require pacdiff
-require mhwd-kernel
+if command -v mhwd-kernel > /dev/null; then
+	echo "This is Manjaro"
+	DISTRO="MANJARO"
+else
+	echo "This is vanilla Arch"
+	DISTRO="ARCH"
+fi
 
-#sudo pacman-key --refresh-keys
+require pikaur
+require pacdiff
+require pacman-key
+
+if [[ "${DISTRO}" == "MANJARO" ]]; then
+	require pacman-mirrors
+	require mhwd-kernel
+else
+	require rankmirrors
+fi
+
+sudo pacman-key --refresh-keys
 pikaur -Syu
-sudo pacman-mirrors -g
+
+if [[ "${DISTRO}" == "MANJARO" ]]; then
+	sudo pacman-mirrors -g
+else
+	sudo cp --verbose /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+	curl -s 'https://archlinux.org/mirrorlist/?country=CA&country=US&protocol=https&use_mirror_status=on' | \
+		sed 's/#Server/Server/' | \
+		rankmirrors -n 10 - | \
+		sed '/^#.*/d' | \
+		sudo tee /etc/pacman.d/mirrorlist
+fi
+
 sudo pacdiff
 sudo pikaur -Rns $(pikaur -Qtdq)
 
-echo "Check for new kernels!"
-
-mhwd-kernel -li
-mhwd-kernel -l
+if [[ "${DISTRO}" == "MANJARO" ]]; then
+	echo "Check for new kernels!"
+	mhwd-kernel -li
+	mhwd-kernel -l
+fi
 
